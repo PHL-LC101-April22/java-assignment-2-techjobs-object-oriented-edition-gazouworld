f56e006c95132da94bc9e79c0d5320f35bff5fc3
Paul Kuhn <pauledwardkuhn@gmail.com>
2022-08-30 19:33:11 -0400

    "final test TDD toString"

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,15 @@
+package org.launchcode.techjobs.oo;
+
+public class JobField {
+
+
+
+
+    public JobField(String value) {
+
+        this();
+        this.value = value;
+    }
+    public string getValue() { return getValue }
+
+}
